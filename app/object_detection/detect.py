20e15e8c3bf8608a0ed8711044c6829d3530f939
richardSHkim <kshtrue95@gmail.com>
2022-09-18 14:13:14 +0900

    take one of 10 frames

--- app/object_detection/detect.py
+++ app/object_detection/detect.py
@@ -34,6 +34,8 @@ def detect(source, status, model, device, half, stride, conf_thres=0.25, iou_thr
 
         t0 = time.time()
         for path, img, im0s, vid_cap in dataset:
+            if (dataset.frame % 10):
+                continue
             img = torch.from_numpy(img).to(device)
             img = img.half() if half else img.float()  # uint8 to fp16/32
             img /= 255.0  # 0 - 255 to 0.0 - 1.0
